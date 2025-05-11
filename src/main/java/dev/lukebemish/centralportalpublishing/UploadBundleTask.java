379dec1cfb56a52abc2c51678226a5a36728b161
Luke Bemish <lukebemish@lukebemish.dev>
2025-05-10 21:57:47 -0500

    Fix inputs

--- src/main/java/dev/lukebemish/centralportalpublishing/UploadBundleTask.java
+++ src/main/java/dev/lukebemish/centralportalpublishing/UploadBundleTask.java
@@ -10,8 +10,11 @@ import org.gradle.api.DefaultTask;
 import org.gradle.api.file.ConfigurableFileCollection;
 import org.gradle.api.file.RegularFileProperty;
 import org.gradle.api.tasks.Input;
+import org.gradle.api.tasks.InputFile;
 import org.gradle.api.tasks.Internal;
 import org.gradle.api.tasks.Nested;
+import org.gradle.api.tasks.PathSensitive;
+import org.gradle.api.tasks.PathSensitivity;
 import org.gradle.api.tasks.TaskAction;
 import org.gradle.api.tasks.UntrackedTask;
 
@@ -33,7 +36,8 @@ import java.util.zip.ZipOutputStream;
 
 @UntrackedTask(because = "This task uploads to a remote repository and thus should not be marked up-to-date.")
 public abstract class UploadBundleTask extends DefaultTask {
-    @Input
+    @InputFile
+    @PathSensitive(PathSensitivity.NAME_ONLY)
     public abstract RegularFileProperty getBundleFile();
 
     @Nested
